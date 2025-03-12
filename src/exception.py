4dcc4dfffd4dd1ac201a0d52d96284f8a16d3cd0
Avadhut Yadav <avadhutyadav01@gmail.com>
2025-03-12 14:22:36 +0530

    error_msg_update

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,15 @@
 import sys
 import logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "error occured in pythonvscript name [{0}] line number [{1}] error message [{2}]".format()
-    file_name,exc_tb.tb_lineno,str(error) 
+    line_number = exc_tb.tb_lineno
+    error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, line_number, str(error)
+    )
+    return error_message 
 
-    return error_message
 
 
 class CustomException(Exception):
